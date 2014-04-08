9b1195aeea9dd5d0e73ef362a5ab46777d947fa9
Ben Birdsall <bbirdsall5@gmail.com>
2014-04-07 22:53:02 -0600

    Added new files that will develop into almost full functionality.

--- AndroidApp/gen/com/example/helloworld/R.java
+++ AndroidApp/gen/com/example/helloworld/R.java
@@ -14,19 +14,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060004;
-        public static final int editText1=0x7f060003;
-        public static final int editText2=0x7f060002;
-        public static final int progressBar1=0x7f060001;
-        public static final int request_result=0x7f060006;
-        public static final int test_label=0x7f060007;
-        public static final int textView1=0x7f060000;
-        public static final int url_input=0x7f060005;
+        public static final int accountName=0x7f060001;
+        public static final int button2=0x7f060005;
+        public static final int email=0x7f060002;
+        public static final int keyGenStatus=0x7f060004;
+        public static final int loginTitle=0x7f060000;
+        public static final int signIn=0x7f060003;
+        public static final int test_label=0x7f060006;
     }
     public static final class layout {
         public static final int activity_create_account=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_transaction=0x7f030002;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_transaction=0x7f030003;
     }
     public static final class string {
         public static final int action_settings=0x7f040003;
---
+++ AndroidApp/src/com/example/helloworld/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+
+}
