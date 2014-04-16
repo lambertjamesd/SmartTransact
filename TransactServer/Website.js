e16235d01617a79fdffebdcebeb72a78ec11410e
James Lambert <lambertjamesd@gmail.com>
2014-04-15 22:31:09 -0600

    Started to make things look good

--- TransactServer/Website.js
+++ TransactServer/Website.js
@@ -6,11 +6,24 @@ function Website(app)
 	app.use(express.static(process.cwd() + '/public'));
 
 	app.get('/', function(request, response) {
-		response.send(jade.renderFile('views/index.jade', {'title':"Smart Transact"}));
+		response.send(jade.renderFile('views/index.jade', {
+			'title':"Smart Transact",
+			'currentURL':'/'
+			}));
+	});
+	
+	app.get('/download', function(request, response) {
+		response.send(jade.renderFile('views/download.jade', {
+			'title':"Smart Transact",
+			'currentURL':'/download'
+			}));
 	});
 	
 	app.get('/example', function(request, response) {
-		response.send(jade.renderFile('views/example.jade', {'title':"Smart Transact"}));
+		response.send(jade.renderFile('views/example.jade', {
+			'title':"Smart Transact",
+			'currentURL':'/example'
+			}));
 	});
 	
 	app.post('/example/deposit', function(request, response) {
